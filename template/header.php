e751c73201f97a40bcb956138957ef474a05b080
IcyAuk <92365145+IcyAuk@users.noreply.github.com>
2024-04-10 12:30:41 +0200

    working on header style

--- template/header.php
+++ template/header.php
@@ -1,14 +1,12 @@
 <?php
 require "lib/config.php";
 require "lib/pdo.php";
-echo "header.php loaded<br>";
+//echo "header.php loaded<br>";
 
 $navbar = [
     ["title" => "Home", "href" => "test.php"],
     ["title" => "Comments", "href" => "test.php"],
-    ["title" => "Articles", "href" => "test.php"],
-    ["title" => "Log in", "href" => "test.php"],
-    ["title" => "Log out", "href" => "test.php"],
+    ["title" => "Articles", "href" => "test.php"]
 ]
 ?>
 
@@ -22,17 +20,25 @@ $navbar = [
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <header class="header">
-        <nav class="navbar">
-            <div>
-                <ul>
+    <header class="header__container">
+
+        <div class="header__logo">
+            <img src="/assets/logoipsum-317.svg" alt="Logo placeholder">
+        </div>
+
+        <nav class="header__navbar">
+
+            <div class="nav__wrapper">
+                <ul class="nav__list">
                     <?php foreach($navbar as $li){?>
-                        <li><?= $li["title"] ?></li>
+                        <li class="header__button"><a href="<?=$li["href"]?>"><?= $li["title"] ?></a></li>
                     <?php } ?>
                 </ul>
             </div>
-            <div>
-            
+
+            <div class="nav__connection">
+                <div class="header__button"><a href="#">Log in</a></div>
+                <div class="header__button"><a href="#">Log out</a></div>
             </div>
         </nav>
     </header>
---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple Header</title>
+  <link rel="stylesheet" href="/css/teststyle.css">
+</head>
+<body>
+  <header>
+    <div class="logo">Your Logo</div>
+    <nav>
+      <ul class="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Articles</a></li>
+        <li><a href="#">Comments</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
